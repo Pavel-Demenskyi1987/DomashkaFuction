534269659050c94b443d76acaa6499b95095802c
Pavel-Demenskyi1987 <149922466+Pavel-Demenskyi1987@users.noreply.github.com>
2023-11-29 01:23:06 +0800

    Подправил

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -9,21 +9,18 @@ class Program
             Console.Write("Введите целое число или 'q' для выхода: ");
             string input = Console.ReadLine();
 
-            // Проверяем, является ли ввод символом 'q'
             if (input.ToLower() == "q")
             {
                 Console.WriteLine("Программа завершена.");
-                break; // Выход из бесконечного цикла
+                break; 
             }
 
-            // Пытаемся преобразовать введенную строку в целое число
             if (int.TryParse(input, out int number))
             {
-                // Проверяем, является ли сумма цифр числа чётной
                 if (IsSumOfDigitsEven(number))
                 {
                     Console.WriteLine($"Сумма цифр числа {number} чётная. Программа завершена.");
-                    break; // Выход из бесконечного цикла
+                    break; 
                 }
                 else
                 {
@@ -39,19 +36,18 @@ class Program
         Console.ReadLine();
     }
 
-    // Функция для проверки, является ли сумма цифр числа чётной
     static bool IsSumOfDigitsEven(int num)
     {
         int sum = 0;
-        num = Math.Abs(num); // Берем модуль числа, чтобы избежать проблем с отрицательными числами
+        num = Math.Abs(num); 
 
         while (num > 0)
         {
-            sum += num % 10; // Добавляем последнюю цифру к сумме
-            num /= 10; // Убираем последнюю цифру из числа
+            sum += num % 10; 
+            num /= 10; 
         }
 
-        return sum % 2 == 0; // Возвращаем true, если сумма чётная
+        return sum % 2 == 0;
     }
 }
 
